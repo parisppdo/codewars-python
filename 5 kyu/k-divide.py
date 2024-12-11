a1526909aa01c75eb216ee489b52932ef9efdbe0
Paris Papadopoulos <parisppdo@protonmail.com>
2024-12-11 14:07:51 +0200

    code works. some tests timed out

--- 5 kyu/k-divide.py
+++ 5 kyu/k-divide.py
@@ -1,20 +1,18 @@
 def calculate_sum(N, K):
-
     divisible = []
     non_divisible = []
     divider = K
 
-    for i in range (1, N + 1):
-        if (i % K) != 0:
-            non_divisible.append(i)
+    for count in range(1, N + 1):
+        if (count % divider) != 0:
+            non_divisible.append(count)
         else:
-            while i % K == 0:
-                divisible.append(i)
-                i = i // divider
+            while count % K == 0:
+                count = count // divider
+            divisible.append(count)
 
     divisible_sum = sum(divisible)
     non_divisible_sum = sum(non_divisible)
 
     return divisible_sum + non_divisible_sum
 
-print(calculate_sum(16, 2))
