df3fed0b620e1a0beb4a90ee208d5cfa72f27f19
Paris Papadopoulos <parisppdo@protonmail.com>
2025-04-19 00:57:08 +0300

    renamed list

--- 6 kyu/Reverse every other word in the string.py
+++ 6 kyu/Reverse every other word in the string.py
@@ -1,8 +1,8 @@
 def reverse_alternate(st):
     cleaned_string = ' '.join(st.strip().split())
-    words_list = cleaned_string.split()
+    words = cleaned_string.split()
 
-    for i in range(1, len(words_list), 2):
-        words_list[i] = words_list[i][::-1]
+    for i in range(1, len(words), 2):
+        words[i] = words[i][::-1]
 
-    return ' '.join(words_list)
+    return ' '.join(words)
